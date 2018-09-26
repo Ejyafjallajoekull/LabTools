ee14438b1cecbc9d97ea2688bd2f85eb3255d271
Planters <30956028+Ejyafjallajoekull@users.noreply.github.com>
2018-09-26 17:40:03 +0200

    Defined modules for cell counting.

---
+++ src/eyja.lab.tools.cell.counter.test/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * Defines a testing module for eyja.lab.tools.cell.counter.
+ * 
+ * @author Planters
+ *
+ */
+module eyja.lab.tools.cell.counter.test {
+	
+	requires java.base;
+	requires eyja.lab.tools.cell.counter;
+	requires central.logging;
+	requires koro.sensei.tester;
+	
+}
---
+++ src/eyja.lab.tools.cell.counter/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * Defines a cell counting module for use in biological laboratories.
+ * 
+ * @author Planters
+ *
+ */
+module eyja.lab.tools.cell.counter {
+	
+	requires java.base;
+	
+}
