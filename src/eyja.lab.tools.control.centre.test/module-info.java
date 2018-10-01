d628c469b96f039b1b2c7524b79f31a91dbcca7b
Planters <30956028+Ejyafjallajoekull@users.noreply.github.com>
2018-10-01 11:15:45 +0200

    Added Control Centre Module
    A module managing the interplay of all LabTools components has been
    defined.

---
+++ src/eyja.lab.tools.control.centre.test/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * Defines a testing module for eyja.lab.tools.control.centre.
+ * 
+ * @author Planters
+ *
+ */
+module eyja.lab.tools.cell.counter.test {
+	
+	requires java.base;
+	requires eyja.lab.tools.control.centre;
+	requires central.logging;
+	requires koro.sensei.tester;
+	
+}
---
+++ src/eyja.lab.tools.control.centre/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * Defines a module managing the interplay of different tools.
+ * 
+ * @author Planters
+ *
+ */
+module eyja.lab.tools.control.centre {
+	
+	requires java.base;
+	requires central.logging;
+	
+}
