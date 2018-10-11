f897c4ff8cce33a2c837851950b35464de17dd8e
Planters <30956028+Ejyafjallajoekull@users.noreply.github.com>
2018-10-11 10:19:49 +0200

    Changed Resource To Abstract Class
    In order to provide some default implementations Resource has been
    converted to an abstract class instead of an interface.

--- src/eyja.lab.tools.control.centre/eyja/lab/tools/control/centre/management/Resource.java
+++ src/eyja.lab.tools.control.centre/eyja/lab/tools/control/centre/management/Resource.java
@@ -1,25 +1,68 @@
 package eyja.lab.tools.control.centre.management;
 
 /**
- * The Resource interface defines serialisation and ID mapping of resources.
+ * The abstract Resource class defines serialisation and ID mapping of resources.
  * 
  * @author Planters
  *
  */
-public interface Resource {
+public abstract class Resource {
+
+	private ResourceID id = null;
 	
 	/**
 	 * Serialise the resource to an array of bytes.
 	 * 
 	 * @return the byte representation of this resource
 	 */
-	public byte[] serialise();
+	public abstract byte[] serialise();
 	
 	/**
 	 * Get the resource's ID unique inside its origin.
 	 * 
 	 * @return the ID of this resource
 	 */
-	public ResourceID getID();
+	public ResourceID getID() {
+		return this.id;
+	}
+	
+	/**
+	 * Set the resource's ID unique inside its origin.
+	 * 
+	 * @param id - the new ID of this resource
+	 */
+	public void setID(ResourceID id) {
+		this.id = id;
+	}
 
+	@Override
+	public String toString() {
+		ResourceID id = this.getID();
+		if (id != null) {
+			return id.toString();
+		} else {
+			return null;
+		}
+	}
+	
+	@Override
+	public int hashCode() {
+		ResourceID id = this.getID();
+		return 31 * 1 + ((id == null) ? 0 : id.hashCode());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj != null && obj instanceof Resource) {
+			ResourceID ownID = this.getID();
+			ResourceID compID = ((Resource) obj).getID();
+			if (ownID == null && compID == null) {
+				return true;
+			} else if (ownID != null) {
+				return ownID.equals(compID);
+			}
+		}
+		return false;
+	}
+	
 }
