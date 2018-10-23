96416eb6855105f1e910532032ed382330d78444
Planters <30956028+Ejyafjallajoekull@users.noreply.github.com>
2018-10-23 22:47:05 +0200

    Implemented Resource References
    A serialisation independent referencing mechanism for resources has been
    resources has been implemented.

---
+++ src/eyja.lab.tools.control.centre.test/eyja/lab/tools/control/centre/test/management/ResourceReferenceTesting.java
@@ -0,0 +1,133 @@
+package eyja.lab.tools.control.centre.test.management;
+
+import eyja.lab.tools.control.centre.management.Origin;
+import eyja.lab.tools.control.centre.management.ResourceID;
+import eyja.lab.tools.control.centre.management.ResourceReference;
+import eyja.lab.tools.control.centre.test.TestRunnerWrapper;
+import koro.sensei.tester.TestFailureException;
+import koro.sensei.tester.TestSubject;
+
+/**
+ * The ResourceReferenceTesting class test the ResourceReference class for correct functionality.
+ * 
+ * @author Planters
+ *
+ */
+public class ResourceReferenceTesting implements TestSubject {
+
+	@Override
+	public void runAllTests() throws TestFailureException {
+		ResourceReferenceTesting.testConstructors();
+		ResourceReferenceTesting.testGetting();
+	}
+	
+	/**
+	 * Test the constructors and some basic equality.
+	 * 
+	 * @throws TestFailureException the test did fail
+	 */
+	private static void testConstructors() throws TestFailureException {
+		for (int i = 0; i < 10000; i++) {
+			{ // test custom definition
+				String testOrigin = TestRunnerWrapper.createRandomString();
+				long testID = TestRunnerWrapper.RANDOM.nextLong();
+				ResourceReference firstRef = new ResourceReference(testOrigin, testID);
+				ResourceReference secondRef = new ResourceReference(testOrigin, testID);
+				TestSubject.assertTestCondition(firstRef.equals(secondRef), 
+						String.format("The resource reference %s should equal %s.", firstRef, secondRef));
+				TestSubject.assertTestCondition(!firstRef.equals(null), 
+						String.format("The resource reference %s should not equal %s.", firstRef, null));
+				secondRef = new ResourceReference(testOrigin, testID + 1l);
+				TestSubject.assertTestCondition(!firstRef.equals(secondRef), 
+						String.format("The resource reference %s should not equal %s.", firstRef, secondRef));
+				secondRef = new ResourceReference(TestRunnerWrapper.createRandomString() + 
+						TestRunnerWrapper.createRandomString(), testID);
+				TestSubject.assertTestCondition(!firstRef.equals(secondRef), 
+						String.format("The resource reference %s should not equal %s.", firstRef, secondRef));
+				// test null origin
+				try {
+					ResourceReference nullRef = new ResourceReference(null, testID);
+					throw new TestFailureException(String.format("The creation of resource reference "
+							+ "%s should fail as no origin is supplied.", nullRef));
+				} catch (NullPointerException e) {
+					// Do nothing as this is expected behaviour.
+				}
+			} { // test resource ID
+				Origin testOrigin = OriginTesting.createRandomOrigin();
+				long testID = TestRunnerWrapper.RANDOM.nextLong();
+				ResourceID testRID = new ResourceID(testOrigin, testID);
+				ResourceReference firstRef = new ResourceReference(testRID);
+				ResourceReference secondRef = new ResourceReference(testRID);
+				TestSubject.assertTestCondition(firstRef.equals(secondRef), 
+						String.format("The resource reference %s should equal %s.", firstRef, secondRef));
+				TestSubject.assertTestCondition(!firstRef.equals(null), 
+						String.format("The resource reference %s should not equal %s.", firstRef, null));
+				secondRef = new ResourceReference(new ResourceID(testOrigin, testID + 1l));
+				TestSubject.assertTestCondition(!firstRef.equals(secondRef), 
+						String.format("The resource reference %s should not equal %s.", firstRef, secondRef));
+				secondRef = new ResourceReference(TestRunnerWrapper.createRandomString() + 
+						TestRunnerWrapper.createRandomString(), testID);
+				TestSubject.assertTestCondition(!firstRef.equals(secondRef), 
+						String.format("The resource reference %s should not equal %s.", firstRef, secondRef));
+				// test null id
+				try {
+					ResourceReference nullRef = new ResourceReference(null);
+					throw new TestFailureException(String.format("The creation of resource reference "
+							+ "%s should fail as no origin is supplied.", nullRef));
+				} catch (NullPointerException e) {
+					// Do nothing as this is expected behaviour.
+				}
+				// test null origin
+				try {
+					ResourceReference nullRef = new ResourceReference(new ResourceID(null, testID));
+					throw new TestFailureException(String.format("The creation of resource reference "
+							+ "%s should fail as no origin is supplied.", nullRef));
+				} catch (NullPointerException e) {
+					// Do nothing as this is expected behaviour.
+				}
+				// test null origin
+				try {
+					ResourceReference nullRef = new ResourceReference(new ResourceID(new Origin(null, null), testID));
+					throw new TestFailureException(String.format("The creation of resource reference "
+							+ "%s should fail as no origin is supplied.", nullRef));
+				} catch (NullPointerException e) {
+					// Do nothing as this is expected behaviour.
+				}
+			}
+			
+		}
+	}
+	
+	/**
+	 * Test getting the different parts of the reference.
+	 * 
+	 * @throws TestFailureException the test did fail
+	 */
+	private static void testGetting() throws TestFailureException {
+		for (int i = 0; i < 10000; i++) {
+			{ // test custom definition
+				String testOrigin = TestRunnerWrapper.createRandomString();
+				long testID = TestRunnerWrapper.RANDOM.nextLong();
+				ResourceReference testRef = new ResourceReference(testOrigin, testID);
+				TestSubject.assertTestCondition(testRef.getID() == testID, 
+						String.format("The ID specifier %s of the resource reference %s should be %s.", 
+								testRef.getID(), testRef, testID));
+				TestSubject.assertTestCondition(testRef.getOrigin() == testOrigin, 
+						String.format("The origin specifier %s of the resource reference %s should be %s.", 
+								testRef.getOrigin(), testRef, testOrigin));
+			} { // test resource ID
+				long testID = TestRunnerWrapper.RANDOM.nextLong();
+				ResourceID testRID = new ResourceID(OriginTesting.createRandomOrigin(), testID);
+				String testOrigin = testRID.getOrigin().getFile().getPath();
+				ResourceReference testRef = new ResourceReference(testRID);
+				TestSubject.assertTestCondition(testRef.getID() == testID, 
+						String.format("The ID specifier %s of the resource reference %s should be %s.", 
+								testRef.getID(), testRef, testID));
+				TestSubject.assertTestCondition(testRef.getOrigin() == testOrigin, 
+						String.format("The origin specifier %s of the resource reference %s should be %s.", 
+								testRef.getOrigin(), testRef, testOrigin));
+			}
+		}
+	}
+	
+}
---
+++ src/eyja.lab.tools.control.centre/eyja/lab/tools/control/centre/management/ResourceReference.java
@@ -0,0 +1,134 @@
+package eyja.lab.tools.control.centre.management;
+
+import java.io.File;
+
+/**
+ * The ResourceReference class references a resource based on the file path of the 
+ * origin it originates from and its unique origin specific ID.
+ * 
+ * @author Planters
+ *
+ */
+public class ResourceReference {
+
+	private final String origin;
+	private final long id;
+	
+	/**
+	 * Create a new reference to a resource by specifying the file path it originates from and its
+	 * ID inside the specified origin. A physical representation of the reference is needed, so the 
+	 * origin may not be null.
+	 * 
+	 * @param origin - the file path of the origin that contains the resource
+	 * @param id - the ID of the resource
+	 * 
+	 * @throws NullPointerException if the origin is null
+	 */
+	public ResourceReference(String origin, long id) {
+		if (origin != null) {
+			this.id = id;
+			this.origin = origin;
+		} else {
+			throw new NullPointerException("An origin without a physical representation cannot be "
+					+ "referenced.");
+		}
+	}
+	
+	/**
+	 * Create a new reference to a resource by specifying its unique resource ID.
+	 * A physical representation of the reference is needed, so the 
+	 * origin of the resource ID may not be null.
+	 * 
+	 * @param id - the ID of the resource
+	 * 
+	 * @throws NullPointerException if the resource ID is null or the origin specified by the ID is 
+	 * invalid
+	 */
+	public ResourceReference(ResourceID id) {
+		if (id != null) {
+			Origin idOrigin = id.getOrigin();
+			if (idOrigin != null) {
+				File f = idOrigin.getFile();
+				if (f != null) {
+					String originPath = f.getPath();
+					if (originPath != null) {
+						this.id = id.getID();
+						this.origin = originPath;
+					} else {
+						throw new NullPointerException("An origin without a physical representation cannot be "
+								+ "referenced.");
+					}
+				} else {
+					throw new NullPointerException(String.format("A valid origin is needed to create a valid "
+							+ "reference, but the origin %s specified by the resource ID %s is not.", 
+							idOrigin, id));
+				}
+			} else {
+				throw new NullPointerException(String.format("An origin is needed to create a valid "
+						+ "reference, but none is specified by the resource ID %s.", id));
+			}
+		} else {
+			throw new NullPointerException("An origin without a physical representation cannot be "
+					+ "referenced.");
+		}
+	}
+	
+	/**
+	 * Set the origin of the resource as path to its physical representation. A physical representation 
+	 * of a reference is needed, so the origin needs to be represented by a valid file.
+	 * 
+	 * @param origin - the path of the origin
+	 * 
+	 * @throws NullPointerException if the path of the origin is null
+	 */
+//	private void setOrigin(String origin) {
+//		if (origin != null) {
+//			this.origin = origin;
+//		} else {
+//			throw new NullPointerException("An origin without a physical representation cannot be "
+//					+ "referenced.");
+//		}
+//	}
+	
+	/**
+	 * Get the file path of the origin containing the resource.
+	 * 
+	 * @return the file path of the origin
+	 */
+	public String getOrigin() {
+		return this.origin;
+	}
+	
+	/**
+	 * Get the origin specific ID of the resource.
+	 * 
+	 * @return the origin specific ID of the referenced resource
+	 */
+	public long getID() {
+		return this.id;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("[%s:%s]", this.getOrigin(), this.getID());
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (this.id ^ (this.id >>> 32));
+		result = prime * result + this.origin.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj != null && obj instanceof ResourceReference) {
+			ResourceReference comp = (ResourceReference) obj;
+			return this.id == comp.id && this.origin.equals(comp.origin);
+		}
+		return false;
+	}
+	
+}
