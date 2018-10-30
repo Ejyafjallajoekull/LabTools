ca110c0785c759adb9324a02986a72cba4e19e0d
Planters <30956028+Ejyafjallajoekull@users.noreply.github.com>
2018-10-30 13:46:08 +0100

    Defined Launch Pad Module
    A module for launching the different tools has been defined.

---
+++ src/eyja.lab.tools.launch.pad/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * Defines a module launching and initialising different tools.
+ * 
+ * @author Planters
+ *
+ */
+module eyja.lab.tools.launch.pad {
+	
+	
+	
+	requires java.base;
+	requires central.logging;
+	requires eyja.lab.tools.control.centre;
+	requires eyja.lab.tools.cell.counter;
+	
+}
