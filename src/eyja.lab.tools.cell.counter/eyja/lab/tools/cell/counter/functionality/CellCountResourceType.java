46840d8c35c62bfab4cffafd09ae64aed4d0353f
Planters <30956028+Ejyafjallajoekull@users.noreply.github.com>
2018-10-31 19:38:12 +0100

    Added Robustness To Cell Count Resource Type Serialisation
    The serialisation of cell count resource types is now transparent and
    more robust.

--- src/eyja.lab.tools.cell.counter/eyja/lab/tools/cell/counter/functionality/CellCountResourceType.java
+++ src/eyja.lab.tools.cell.counter/eyja/lab/tools/cell/counter/functionality/CellCountResourceType.java
@@ -8,9 +8,38 @@ package eyja.lab.tools.cell.counter.functionality;
  */
 public enum CellCountResourceType {
 
-	COUNT,
-	DILUTION,
-	COUNTED_DILUTION,
-	COUNTING_SERIES
+	COUNT((byte) 0),
+	DILUTION((byte) 1),
+	COUNTED_DILUTION((byte) 2),
+	COUNTING_SERIES((byte) 3);
 	
+	private byte identifier = -1;
+	
+	private CellCountResourceType(byte identifier) {
+		this.identifier = identifier;
+	}
+	
+	/**
+	 * Infer a binary representation of the according cell count resource type.
+	 * 
+	 * @return the resource type's binary representation
+	 */
+	public byte[] toBytes() {
+		return new byte[] {this.identifier};
+	}
+	
+	/**
+	 * Infer the cell count resource type from binary data.
+	 * 
+	 * @param data - the binary data identifying the cell count resource type
+	 * @return the corresponding resource type
+	 */
+	public static CellCountResourceType fromByte(byte data) {
+		for (CellCountResourceType v : CellCountResourceType.values()) {
+			if (v.identifier == data) {
+				return v;
+			}
+		}
+		return null;
+	}
 }
