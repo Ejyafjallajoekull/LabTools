1f6b5cf0eec02157ebcd29a43f54d4a8bf10bc95
Planters <30956028+Ejyafjallajoekull@users.noreply.github.com>
2018-11-13 17:14:54 +0100

    Revised Cached References
    Cached references have been fully implemented and revised.

--- src/eyja.lab.tools.control.centre/eyja/lab/tools/control/centre/management/CachedReference.java
+++ src/eyja.lab.tools.control.centre/eyja/lab/tools/control/centre/management/CachedReference.java
@@ -5,32 +5,43 @@ package eyja.lab.tools.control.centre.management;
  * 
  * @author Planters
  *
- * @param <T> - the type of resource expected
  */
-public class CachedReference<T extends Resource> {
+public class CachedReference extends ResourceReference {
 
-	private T cachedResource = null;
-	boolean isCached = false;
-	private final ResourceReference referenceToResource;
-	private final Class<T> type;
+	private Resource cachedResource = null;
+	private boolean isCached = false;
 	
 	/**
-	 * Create a new cached reference from the specified resource reference.
+	 * Create a new cached reference to a resource by specifying the file path it originates from and its
+	 * ID inside the specified origin. 
 	 * Thereby the resource referenced can be cached and must not be dereferenced from 
 	 * the according handler for every access.
+	 * A physical representation of the reference is needed, so the 
+	 * origin may not be null.
 	 * 
-	 * @param reference - the resource reference to cache
-	 * @param type - the type of the reference
-	 * @throws NullPointerException if the specified reference or type is null
+	 * @param origin - the file path of the origin that contains the resource
+	 * @param id - the ID of the resource
+	 * 
+	 * @throws NullPointerException if the origin is null
 	 */
-	public CachedReference(ResourceReference reference, Class<T> type) {
-		if (reference != null && type != null) {
-			this.referenceToResource = reference;
-			this.type = type;
-		} else {
-			throw new NullPointerException("Null cannot be dereferenced, thereby caching the "
-					+ "according resource is impossible.");
-		}
+	public CachedReference(String origin, long id) {
+		super(origin, id);
+	}
+
+	/**
+	 * Create a new cached reference to a resource by specifying its unique resource ID.
+	 * Thereby the resource referenced can be cached and must not be dereferenced from 
+	 * the according handler for every access.
+	 * A physical representation of the reference is needed, so the 
+	 * origin of the resource ID may not be null.
+	 * 
+	 * @param id - the ID of the resource
+	 * 
+	 * @throws NullPointerException if the resource ID is null or the origin specified by the ID is 
+	 * invalid
+	 */
+	public CachedReference(ResourceID id) {
+		super(id);
 	}
 	
 	/**
@@ -39,19 +50,46 @@ public class CachedReference<T extends Resource> {
 	 * 
 	 * @param fallbackHandler - the handler to dereference the reference from if it has not 
 	 * already been cached
+	 * 
 	 * @return the referenced resource
 	 * @ throws NullPointerException if the resource is not cached yet and the origin handler 
 	 * for dereferencing is null
+	 * 
 	 * @throws ReferenceException if the type of the resource does not match the type of this
 	 * reference while caching
 	 * @throws IllegalArgumentException if the reference origin is not managed by the specified 
 	 * handler while caching
 	 */
-	public T getResource(OriginHandler fallbackHandler) throws ReferenceException {
+	public Resource getResource(OriginHandler fallbackHandler) throws ReferenceException {
 		if(!this.isCached()) {
 			this.cache(fallbackHandler);			
 		}
 		return this.getCachedResource();
+	}
+	
+	/**
+	 * Get the resource referenced by this reference. If the resource has not been cached, it 
+	 * will be dereferenced from the specified handler. Otherwise the cached resource is returned.
+	 * 
+	 * @param <T> - the expected type
+	 * @param fallbackHandler - the handler to dereference the reference from if it has not 
+	 * already been cached
+	 * @param expectedType - the class of the expected type
+	 * 
+	 * @return the referenced resource
+	 * 
+	 * @ throws NullPointerException if the resource is not cached yet and the origin handler 
+	 * for dereferencing is null
+	 * @throws ReferenceException if the type of the resource does not match the type of this
+	 * reference after caching
+	 * @throws IllegalArgumentException if the reference origin is not managed by the specified 
+	 * handler while caching
+	 */
+	public <T> T getResource(OriginHandler fallbackHandler, Class<T> expectedType) throws ReferenceException {
+		if(!this.isCached()) {
+			this.cache(fallbackHandler);			
+		}
+		return this.getCachedResource(expectedType);
 	}
 	
 	/**
@@ -68,29 +106,16 @@ public class CachedReference<T extends Resource> {
 	 * previously cached. All preexisting caching data will be overwritten.
 	 * 
 	 * @param referenceHandler - the handler to use for caching
+	 * 
 	 * @throws NullPointerException if the origin handler 
 	 * for dereferencing is null
-	 * @throws ReferenceException if the type of the resource does not match the type of this
-	 * reference
 	 * @throws IllegalArgumentException if the reference origin is not managed by the specified 
 	 * handler
 	 */
-	@SuppressWarnings("unchecked")
 	public void cache(OriginHandler referenceHandler) throws ReferenceException {
 		if (referenceHandler != null) {
-			try {
-				Resource a = referenceHandler.dereference(referenceToResource);
-				this.cachedResource = this.convert(a);
-				System.out.println(String.format("%s /// %s", a.getClass(), ((T) a).getClass()));
-
-//				this.cachedResource = (T) referenceHandler.dereference(referenceToResource);
-				this.isCached = true;
-			} catch (ClassCastException e) {
-				System.out.println("Trigger");
-				throw new ReferenceException(String.format("The resource %s dereferenced from "
-						+ " handler %s is not of the expected type.", 
-						referenceHandler.dereference(referenceToResource), referenceHandler), e);
-			}
+			this.cachedResource = referenceHandler.dereference(this);
+			this.isCached = true;
 		} else {
 			throw new NullPointerException(String.format("Cannot cache reference %s "
 					+ "from a null handler.", this));
@@ -103,31 +128,50 @@ public class CachedReference<T extends Resource> {
 	 * 
 	 * @return the currently cached resource.
 	 */
-	public T getCachedResource() {
+	public Resource getCachedResource() {
 		return this.cachedResource;
 	}
 	
 	/**
-	 * Get the resource reference used for caching.
+	 * Get the cached resource this reference is pointing to. This will return null 
+	 * if no caching has been performed yet.
+	 * @param <T> - the expected type
+	 * @param expectedType - the class of the expected type
 	 * 
-	 * @return the underlying resource reference
+	 * @return the currently cached resource
+	 * 
+	 * @throws NullPointerException if the expected type is null
+	 * @throws ReferenceException if the cached resource is not of the expected type
 	 */
-	public ResourceReference getReference() {
-		return this.referenceToResource;
+	public <T> T getCachedResource(Class<T> expectedType) throws ReferenceException {
+		if (expectedType != null) {
+			if (this.cachedResource != null) {
+				if (expectedType.isInstance(this.cachedResource)) {
+					return expectedType.cast(this.cachedResource);
+				} else {
+					throw new ReferenceException(String.format("The resource %s cached is of type %s, "
+							+ "but type %s is expected.", this.cachedResource, 
+							this.cachedResource.getClass(), expectedType));
+				}
+			} else {
+				return null;
+			}
+		} else {
+			throw new NullPointerException("The expected type of a reference cannot be null.");
+		}
 	}
 	
 	@Override
 	public String toString() {
-		return String.format("[Chached: %s ; %s]", this.isCached(), this.getReference());
+		return String.format("[%s:%s:%s]", this.getOrigin(), this.getID(), this.isCached());
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
+		int result = super.hashCode();
 		result = prime * result + ((this.cachedResource == null) ? 0 : this.cachedResource.hashCode());
 		result = prime * result + (this.isCached ? 1231 : 1237);
-		result = prime * result + this.referenceToResource.hashCode();
 		return result;
 	}
 
@@ -135,11 +179,10 @@ public class CachedReference<T extends Resource> {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
+		if (!super.equals(obj))
 			return false;
 		if (this.getClass() != obj.getClass())
 			return false;
-		@SuppressWarnings("rawtypes")
 		CachedReference other = (CachedReference) obj;
 		if (this.cachedResource == null) {
 			if (other.cachedResource != null)
@@ -147,8 +190,6 @@ public class CachedReference<T extends Resource> {
 		} else if (!this.cachedResource.equals(other.cachedResource))
 			return false;
 		if (this.isCached != other.isCached)
-			return false;
-		if (!this.referenceToResource.equals(other.referenceToResource))
 			return false;
 		return true;
 	}
